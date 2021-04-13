0cf2b6ba78d884992c5a7d947f145b8c49305cd6
Renan <renanrambuls@gmail.com>
2021-04-13 09:16:41 -0300

    params query body

--- serverr/server.js
+++ serverr/server.js
@@ -7,23 +7,43 @@ const app = express(); //Config para carregar o express
 // http://meusite.com/ <- GET -> Entregue a página
 // http://meusite.com/sobre <- GET -> Entregue a página
 
+app.use(
+    express.urlencoded(
+        {
+            extended: true
+        }
+    )
+);
+
+
 app.get('/', (req, res) => { //O cliente faz uma requisição o servidor entrega uma resposta
+    //O name do input é como a chave do objeto
     res.send(`
     <form action="/" method="POST">
-    Nome: <input type="text" name="nome"
+    Nome do Cliente: <input type="text" name="nome"> 
     <button>Enviar</button>
     </form>
-    `)
+    `);
 });
 
 app.post('/', (req, res) => {
-    res.send('Recebi o formulário')
+    console.log(req.body)
+    res.send(`O que você me enviou foi: ${req.body.nome}`)
 });
 
+app.get('/testes/:idUsuarios?/>parametro?', (req, res) => {
+    //params são as partes que vem na rota da URL /profiles/3
+    //query /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+    //body pega os parametros pelo body
+    
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params.idUsuarios)
+})
 
-app.get('/contato', (req, res) => {
+/*app.get('/contato', (req, res) => {
     res.send('Obrigado por entrar em contato com a gente')
-});
+});*/
 
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000')
