e6e55fe8bda9dd4423d2c35d19a38a36ced9a1be
Renan <renanrambuls@gmail.com>
2021-04-13 11:40:41 -0300

    PAdronização Modelo e Ajustes

--- serverr/server.js
+++ serverr/server.js
@@ -9,16 +9,10 @@ const path = require('path')
 // http://meusite.com/ <- GET -> Entregue a página
 // http://meusite.com/sobre <- GET -> Entregue a página
 
-app.use(
-    express.urlencoded(
-        {
-            extended: true
-        }
-    )
-);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'))//Pasta de conteudo estatico
 app.set('views', './src/views');
 app.set('view engine', 'ejs')
-
 app.use(routes);
 
 /*
