8bf6a88b2983804432be73bb27ec1925924f5c11
Renan <renanrambuls@gmail.com>
2021-04-16 11:21:17 -0300

    Login e sessão

--- projeto-agenda/src/controllers/loginController.js
+++ projeto-agenda/src/controllers/loginController.js
@@ -2,13 +2,42 @@ const Login = require('../models/LoginModel');
 const login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-  res.render('login');
+  if(req.session.user) return res.render('login-logado')
+  return res.render('login')
 };
 
 exports.register = async function(req, res) {
   try{
     const login = new Login(req.body);
     await login.register();
+  
+    if(login.errors.length > 0){
+      req.flash('errors', login.errors)
+      req.session.save(function(){
+      return  res.redirect('back');
+      });
+      return;
+    }
+    
+    req.flash('success', 'Você entrou no sistema')
+    req.session.user = login.user;
+    req.session.save(function(){
+    return  res.redirect('back');
+    });
+
+    return res.send(login.errors);
+  } catch(e){
+    console.log(e);
+  return res.render('404');
+  }
+
+
+  res.send(login.errors);
+};
+exports.login = async function(req, res) {
+  try{
+    const login = new Login(req.body);
+    await login.login();
   
     if(login.errors.length > 0){
       req.flash('errors', login.errors)
@@ -30,4 +59,9 @@ exports.register = async function(req, res) {
 
 
   res.send(login.errors);
+};
+
+exports.logout = function(req, res){
+  req.session.destroy();
+  res.redirect('/');
 };
